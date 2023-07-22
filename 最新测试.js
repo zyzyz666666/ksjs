c556c22d45e7c6ad16a8fb57ad1acc317ec7ca1a
zyzyz666666 <114156742+zyzyz666666@users.noreply.github.com>
2023-07-22 11:38:57 +0800

    Update 最新测试.js

--- 最新测试.js
+++ 最新测试.js
@@ -10,7 +10,7 @@ var window = floaty.window(
 var filePathksjs = '/sdcard/ksjs.js';
 
 
-window.setPosition(device.width / 3, 0);
+window.setPosition(device.width / 3, -100);
 window.setSize(device.width * 1 / 2, 250);
 window.setAdjustEnabled(false);
 
